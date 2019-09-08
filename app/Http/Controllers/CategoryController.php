bbeb3058ff41e2103dbbd3ec092de35c931301bd
ANNA <anutik88@yandex.ru>
2019-09-08 16:30:28 +0300

    Change status field

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,14 +11,14 @@ class CategoryController extends Controller
     public function index($slug)
     {
         $category = Category::where('slug', $slug)->with('products')->firstOrFail();
-        $products = Product::where('status', 1)->where('category_id', $category->id)->paginate(2);
+        $products = Product::where('is_active', 1)->where('category_id', $category->id)->paginate(2);
 
         return view('front.products-list', compact('category', 'products'));
     }
     public function productList($slug) {
         $category = Category::where('slug', $slug)->firstOrFail();
 
-        $products = Product::where('status', 1)
+        $products = Product::where('is_active', 1)
           ->where('category_id', $category->id)->paginate(5);
 
         return view('front.product-list',
